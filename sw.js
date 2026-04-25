e8d3486f5658d56b97f33d9c76e4db26652b1fd6
Aleco <aleco@skalette.com>
2026-04-25 17:40:03 +0200

    Bump service worker cache to v4 e cleanup cache vecchie

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'skalette-v3';
+const CACHE_NAME = 'skalette-v4';
 const urlsToCache = [
   './',
   './index.html',
@@ -9,9 +9,21 @@ const urlsToCache = [
 
 // Install
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate - elimina le cache vecchie
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
